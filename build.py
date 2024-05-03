e8f06524bced1e80d033c869a536f4478d647f7b
grey-cat-1908 <grey.cat1908@gmail.com>
2024-05-03 21:18:46 +0300

    am am am am

--- build.py
+++ build.py
@@ -1,11 +1,12 @@
 import os
 import shutil
+import json
 
 import mistune
 
 render = mistune.create_markdown(
     plugins=['math', 'strikethrough', 'footnotes', 'table', 'url', 'task_lists', 'abbr', 'mark', 'subscript', 'spoiler']
-    )
+)
 
 try:
     shutil.rmtree("build")
@@ -14,18 +15,40 @@ except:
 
 os.mkdir("build")
 
-
 template_file = open('template.html', "r")
 template_text = template_file.read()
 template_file.close()
 
+base_meta_file = open('meta.json', "r")
+base_meta = json.loads(base_meta_file.read())
+base_meta_file.close()
 
-def gen_file(directory):
-    file = open(directory, "r")
+def parse_meta(data):
+    result = ""
+
+    for key, value in data['tags'].items():
+        result += f'<meta name="{key}" content="{value}">\n'
+    
+    for key, value in data['og'].items():
+        result += f'<meta property="og:{key}" content="{value}">\n'
+
+    return result
+
+def gen_file(directory, filename):
+    file = open(directory + filename, "r")
     md_text = file.read()
     file.close()
 
-    return template_text.replace("{{%CONTENT%}}", render(md_text))
+    try:
+        file = open(directory + 'meta.json', "r")
+        meta = json.loads(file.read())
+        file.close()
+    except:
+        meta = base_meta
+    
+    meta_data = parse_meta(meta)
+
+    return template_text.replace("{{%CONTENT%}}", render(md_text)).replace("{{%TITLE%}}", meta['title']).replace("{{%META%}}", meta_data)
 
 def go_through(directory):
     for filename in os.listdir(directory):
@@ -36,13 +59,18 @@ def go_through(directory):
             os.makedirs(f'build/{fier}{filename}')
             go_through(directory + "/" + filename)
         else:
+            if filename.split(".")[1] == "json":
+                continue
             for ofn in os.listdir("static"):
-                if len(ofn.split(".")) == 1:
-                    shutil.copytree(f"{os.getcwd()}/static/{ofn}", f"{os.getcwd()}/build/{fier}{ofn}")
-                else:
-                    shutil.copy(f"{os.getcwd()}/static/{ofn}", f"{os.getcwd()}/build/{fier}{ofn}")
+                try:
+                    if len(ofn.split(".")) == 1:
+                        shutil.copytree(f"{os.getcwd()}/static/{ofn}", f"{os.getcwd()}/build/{fier}{ofn}")
+                    else:
+                        shutil.copy(f"{os.getcwd()}/static/{ofn}", f"{os.getcwd()}/build/{fier}{ofn}")
+                except:
+                    pass
 
-            content = gen_file(f"{os.getcwd()}/{directory}/{filename}")
+            content = gen_file(f"{os.getcwd()}/{directory}/", filename)
             loc = fier + filename.split(".")[0] + '.html'
 
             file = open(f"{os.getcwd()}/build/{loc}", "a")    
